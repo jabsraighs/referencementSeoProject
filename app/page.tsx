f7a8325656c224000928a182ed8134a51349c560
jabsraughs <204bouaki.arthur@gmail.com>
2024-05-22 14:42:17 +0200

    modif errror prod v1

--- app/page.tsx
+++ app/page.tsx
@@ -27,13 +27,13 @@ return (
      
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+          <Image className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt=" " />
         </a>
         <div className="p-5">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Les Terrains Confus: Naviguer dans l&apos;Incertitude </h5>
           </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">La notion de "terrains confus" réfère aux zones d&apos;incertitude ou de complexité dans divers domaines, qu&apos;il s&apos;agisse de technologie, de droit, de psychologie ou de tout autre champ.</p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">La notion de &quot;terrains confus&quot; réfère aux zones d&apos;incertitude ou de complexité dans divers domaines, qu&apos;il s&apos;agisse de technologie, de droit, de psychologie ou de tout autre champ.</p>
           <a href="/articleOne" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -44,7 +44,7 @@ return (
       </div>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+          <Image className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
         </a>
         <div className="p-5">
           <a href="#">
@@ -61,7 +61,7 @@ return (
       </div>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+          <Image className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
         </a>
         <div className="p-5">
           <a href="#">
@@ -78,7 +78,7 @@ return (
       </div>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+          <Image className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
         </a>
         <div className="p-5">
           <a href="#">     
@@ -95,7 +95,7 @@ return (
       </div>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+          <Image className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
         </a>
         <div className="p-5">
           <a href="#">
@@ -112,7 +112,7 @@ return (
       </div>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+          <Image className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
         </a>
         <div className="p-5">
           <a href="#">
