3a32b2cbe54e9cd5ad2a77f410aa70c32929c9b4
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-05-25 16:00:04 +0200

    Added navbar layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar transparent />
         <header>
           <nav className="">
             <ul className="flex flex-row space-x-10 place-content-center">
